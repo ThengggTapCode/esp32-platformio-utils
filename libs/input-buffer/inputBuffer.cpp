34c9d04d1242e5c1de21a5f8c24de408e3798079
ThengggTapCode <samthang6b@gmail.com>
2026-07-13 01:13:27 +0700

    added esp32-s3-n16r8 platformio.ini file, input-buffer library

---
+++ libs/input-buffer/inputBuffer.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include "inputBuffer.h"
+
+String inputBuffer(bool sensitiveInfo) {
+    String bufferedInput = "";
+    while (true) {
+        if (Serial0.available() > 0) {
+            char ch = Serial0.read(); // save every single character in each iteration
+            // when ch is the enter key
+            if (ch == '\r' || ch == '\n') {
+                if (bufferedInput.length() > 0) {                
+                    Serial0.println();
+                    return bufferedInput;
+                }
+            }
+            // when ch is the backspace key
+            else if (ch == 8 || ch == 127) {
+                if (bufferedInput.length() > 0) {
+                    bufferedInput.remove(bufferedInput.length() - 1); // removes the last character
+
+                    // move cursor left by one character, then overwrites the character in that position with a space
+                    Serial0.print("\b \b");
+                }
+            }
+            // when ch is other keys
+            else {
+                bufferedInput += ch;
+                
+                // prints out '*' if the input is a sensitive information
+                char printChar = (sensitiveInfo) ? '*' : ch;
+                Serial0.print(printChar);
+            }
+            delay(5);
+        }
+    }
+}
