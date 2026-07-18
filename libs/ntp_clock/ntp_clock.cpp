60065162b5a979957d31c8bf9dbb665543e4f368
ThengggTapCode <samthang6b@gmail.com>
2026-07-18 22:30:46 +0700

    updated ntp_clock library: uses class NTPClock from now

--- libs/ntp_clock/ntp_clock.cpp
+++ libs/ntp_clock/ntp_clock.cpp
@@ -1,17 +1,75 @@
 #include <Arduino.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h>
+#include <time.h>
 #include "wifi_connection.h"
 #include "ntp_clock.h"
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
 
-void ntpClockSetup() {
+void NTPClock::clockUpdate() {
+    if (timeClient.update()) Serial0.println("[NTPClient] Update successful!");
+    time_t epochTime = timeClient.getEpochTime();
+    struct tm *ptm = gmtime((const time_t *)&epochTime);
+    
+    this->dayOfMonth = ptm->tm_mday;
+    this->dayOfWeek = ptm->tm_wday + 1;
+    this->month = ptm->tm_mon + 1;
+    this->year = ptm->tm_year + 1900;
+    this->hour = timeClient.getHours();
+    this->minute = timeClient.getMinutes();
+    this->second = timeClient.getSeconds();
+}
+
+void NTPClock::clockForceUpdate() { timeClient.forceUpdate(); }
+
+void NTPClock::setup() {
     WiFi_Connection wifi = WiFi_Connection(15);
     while (true) {
         wifi.connect();
         if (wifi.isConnected()) break;
     }
     timeClient.begin();
+    clockForceUpdate();
+}
+
+String NTPClock::formatUInt8Type(uint8_t data) {
+    if (data >= 10) return String(data);
+    return "0" + String(data);
+}
+
+String NTPClock::getDayOfWeek(bool shortDate, bool longDate) {
+    if (shortDate && longDate)
+    return "[ERROR] Cannot choose both short date and long date!";
+    if (!shortDate && !longDate)
+    return "[ERROR] Please choose at least one and only one format!";
+    
+    uint8_t index = this->dayOfWeek - 1;
+    if (index < 0 || index > 6) return "[ERROR] Invalid day in week!";
+
+    if (shortDate) {
+        const char* days[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        return String(days[index]);
+    }
+    const char* days[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    return String(days[index]);
+}
+
+String NTPClock::getDayOfMonth() { return formatUInt8Type(this->dayOfMonth); }
+String NTPClock::getMonth() { return formatUInt8Type(this->month); }
+String NTPClock::getYear() { return String(this->year).c_str(); }
+String NTPClock::getHour() { return formatUInt8Type(this->hour); }
+String NTPClock::getMinute() { return formatUInt8Type(this->minute); }
+String NTPClock::getSecond() { return formatUInt8Type(this->second); }
+
+
+NTPClock::NTPClock() {
+    this->dayOfMonth = 0;
+    this->dayOfWeek = 0;
+    this->month = 0;
+    this->year = 0;
+    this->hour = 0;
+    this->minute = 0;
+    this->second = 0;
 }
--- libs/ntp_clock/ntp_clock.h
+++ libs/ntp_clock/ntp_clock.h
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h>
+#include <time.h>
 #include "wifi_connection.h"
 
 #define NTP_OFFSET 7*3600 // GMT+7
@@ -8,4 +9,24 @@
 #define NTP_ADDRESS "pool.ntp.org" // NTP server address
 
 extern NTPClient timeClient;
-void ntpClockSetup();
+
+class NTPClock {
+private:
+    uint8_t dayOfWeek, dayOfMonth, month;
+    uint8_t hour, minute, second;
+    int year;
+    String formatUInt8Type(uint8_t data);
+    void clockForceUpdate();
+    
+public:
+    NTPClock();
+    void setup();
+    void clockUpdate();
+    String getDayOfWeek(bool shortDate, bool longDate);
+    String getDayOfMonth();
+    String getMonth();
+    String getYear();
+    String getHour();
+    String getMinute();
+    String getSecond();
+};
